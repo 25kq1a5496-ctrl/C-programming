1a2f64445d3d4445b420e18d78065382a427d4e2
25kq1a5496-ctrl <25kq1a5496@pace.ac.in>
2025-11-26 16:59:56 +0530

    Add files via upload

---
+++ jumping statement break.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main()
+{
+   int i;
+   for(i=1;i<=5;i++)
+   {
+      if(i>4)
+      break;
+      printf("%d ",i);
+   }
+    return 0;
+}
+ 
+                                                                                                            
+
+
+
+
+
+
+
+
+
+
+
+
+    
