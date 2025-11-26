26efb134ddecddcd1c8aba4a4c195c5f453cea30
25kq1a5496-ctrl <25kq1a5496@pace.ac.in>
2025-11-26 17:01:57 +0530

    Add files via upload

---
+++ array.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main()
+{
+   int i,a[5];
+   printf("enter 5 elements to continue:\n");
+   for(i=0;i<=4;i++)
+       scanf("%d ",&a[i]);
+   printf("\n given array elements are:\n");
+   for(i=0;i<=4;i++)    
+      printf("%d ",a[i]);
+    return 0;
+}
+ 
+                                                                                                            
+
+
+
+
+
+
+
+
+
+
+
+
+    
