5398d0a722c3cbad1a11fb49a7f7f7935bdc389c
25kq1a5496-ctrl <25kq1a5496@pace.ac.in>
2025-11-26 17:02:48 +0530

    Add files via upload

---
+++ array garbage.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main()
+{
+   int i, a[i];
+    printf ("%d",a[i]);
+   
+    return 0;
+}
+ 
+                                                                                                            
+
+
+
+
+
+
+
+
+
+
+
+
+    
